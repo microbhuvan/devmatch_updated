abf4d833c1bfe9110ae350887b206d267e78da69
microbhuvan <microbhuvan@gmail.com>
2026-07-24 20:14:15 +0530

    Removed bugs

--- backend/dist/routes/payment.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const payment_controller_1 = require("../controllers/payment.controller");
-const paymentRouter = (0, express_1.Router)();
-paymentRouter.post("/create_order", auth_middleware_1.authMiddleware, payment_controller_1.createOrder);
-paymentRouter.post("/verify_payment", auth_middleware_1.authMiddleware, payment_controller_1.verifyPayment);
-exports.default = paymentRouter;
--- backend/dist/validators/request.validator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendRequestFields = void 0;
-exports.validateFields = validateFields;
-exports.sendRequestFields = ["interested", "ignored"];
-function validateFields(status) {
-    return exports.sendRequestFields.includes(status);
-}
